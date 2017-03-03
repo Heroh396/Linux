221e3ebd1c584abc1b6ff27fc6712e994a6cf355
Huy Hung Ho <heroh396@gmail.com>
2017-03-03 22:28:30 +0700

    Add crontab

---
+++ Bash-conf/Crontab.sh
@@ -0,0 +1,13 @@
+################################################################################
+# Project name   :
+# File name      : Crontab.sh
+# Created date   : Sat 25 Feb 2017
+# Author         : Huy Hung Ho
+# Last modified  : Sat 25 Feb 2017
+# Guide          :
+###############################################################################
+#!/bin/bash
+
+sleep 1 && xset dpms force off &&  gnome-screensaver-command -a
+
+
---
+++ Bash-conf/vd.sh
@@ -0,0 +1,13 @@
+################################################################################
+# Project name   :
+# File name      :
+# Created date   : !!DATE
+# Author         : Huy Hung Ho
+# Last modified  : !!DATE
+# Guide          :
+###############################################################################
+#!/bin/bash
+export DISPLAY=:0.0
+sleep 1 && xset dpms force suspend && xdotool key Ctrl+alt+l
+#notify-send "DKM"
+#gnome-screensaver-command -a
